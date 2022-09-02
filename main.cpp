c67ca3a7b348eabbd1c8d3aa1817a8f14f9fe83f
Chris <>
2022-09-03 09:26:48 +1000

    WIP tick function
    Moving over movement, animation and rendering code

--- main.cpp
+++ main.cpp
@@ -36,6 +36,43 @@ void Character::setScreenPos(int winWidth, int winHeight)
 	};
 }
 
+void Character::tick(float deltatime)
+{
+	Vector2 direction{};
+
+	// Movement
+	if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+	if (IsKeyDown(KEY_D)) direction.x += 1.0;
+	if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+	if (IsKeyDown(KEY_S)) direction.y += 1.0;
+
+	if (Vector2Length(direction) != 0.0)
+	{
+		// Set worldPos = worldPos + direction
+		// Move the map
+		worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+
+		// Switch direction
+		direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
+
+		// Change animation 
+		texture = run;
+	}
+	else
+	{
+		texture = idle;
+	}
+
+	// Update Animation frame
+	runningTime += deltatime;
+	if (runningTime >= updateTime)
+	{
+		frame++;
+		runningTime = 0;
+		if (frame > maxFrames)frame = 0;
+	}
+}
+
 int main() 
 {
 	const int windowWidth = 384;
